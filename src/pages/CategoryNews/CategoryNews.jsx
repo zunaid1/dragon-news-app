80b56176ecf849854a0075ad9a0cb8a824bfd17f
zunaid1 <zunaidctg11@gmail.com>
2025-05-01 11:37:43 +0600

    end of 7

--- src/pages/CategoryNews/CategoryNews.jsx
+++ src/pages/CategoryNews/CategoryNews.jsx
@@ -1,12 +1,40 @@
-import React from 'react';
-import { useParams } from 'react-router';
+import React, { useEffect, useState } from 'react';
+import { useLoaderData, useParams } from 'react-router';
 
 const CategoryNews = () => {
-	const {id}= useParams()
+	const { id } = useParams();
+	const data = useLoaderData();
+	const [categoryNews, setCategoryNews] = useState([]);
+
+	//console.log(id, data);
+	useEffect(() => {
+		//let filteredNews=[]
+
+		if (id == "0") {
+			setCategoryNews(data);
+			//console.log(filteredNews);
+			return;
+		}
+		else if(id == "1"){
+			 const filteredNews = data.filter((news) => news.others.is_today_pick == true);
+			 setCategoryNews(filteredNews);
+			 console.log(filteredNews);
+			 return;
+		}
+		else{
+			const filteredNews = data.filter((news) => news.category_id == id)
+			setCategoryNews(filteredNews);
+			console.log(filteredNews);
+		}
+		
+	
+
+	}, [data, id])
+
 
 	return (
 		<div>
-			Category News : {id}
+			Total News for Selected Category {categoryNews.length}
 		</div>
 	);
 };
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -16,7 +16,8 @@ const router = createBrowserRouter(
 				},
 				{
 					path: "/category/:id",
-					Component: CategoryNews
+					Component: CategoryNews, 
+					loader: () => fetch("/news.json"),
 				}
 			]
 		},
